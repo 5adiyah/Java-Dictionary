024c7fc1a1dbee61490043ca362bc35b3a2325c2
sadiyah <sadiyah-ali@hotmail.com>
2016-04-29 09:17:47 -0700

    Initial Commit, file structure set up

---
+++ src/test/java/WordTest.java
@@ -0,0 +1,19 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class name {
+
+  @Test
+public void methodWeAreTesting_behaviorWeAreTesting_expectedResult() {
+  MainClassName testMainClassName = new MainClassName();
+  String expected = "This" ;
+  assertEquals(expected, testMainClassName.methodName("test"));
+  }
+}
+
+//Instantiates correctly
+// get Word
+// get id
+// add Definition parameters = (Definition definition)
+  private ArrayList<Definition> mDefinitions;
+// clear function
